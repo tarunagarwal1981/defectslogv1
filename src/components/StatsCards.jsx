7e4c56d2e263796639b53709e1bd40038fa6f09c
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-15 19:35:33 +0530

    Update StatsCards.jsx

--- src/components/StatsCards.jsx
+++ src/components/StatsCards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from "@/components/ui/card";
+import { Card } from "./ui/card"; 
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { PieChart, Pie, Cell } from 'recharts';
 
